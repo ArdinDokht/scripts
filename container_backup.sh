3108c68804343bb334d7261698fa1a98c2209cfd
ardin <64200985+ArdinDokht@users.noreply.github.com>
2024-11-13 14:55:19 +0330

    Update container_backup.sh
    save to new location in same server !

--- container_backup.sh
+++ container_backup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-CONTAINER_NAME="313490859a8d"
+CONTAINER_ID="313490859a8d"
 NEW_IMAGE_NAME="nginx_proxy_db:21-8-1403"
+EXPORT_FILE="/tmp/nginx_db_image_backup.tar"
 
-docker commit "$CONTAINER_NAME" "$NEW_IMAGE_NAME"
-echo "Container $CONTAINER_NAME committed as image $NEW_IMAGE_NAME"
+docker commit "$CONTAINER_ID" "$NEW_IMAGE_NAME"
+echo "Container $CONTAINER_ID committed as image $NEW_IMAGE_NAME"
+
+
+docker save -o "$EXPORT_FILE" "$NEW_IMAGE_NAME"
+echo "Image $NEW_IMAGE_NAME saved as $EXPORT_FILE"
