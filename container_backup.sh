aa55fe1dec9f50c8edc7afe5f146eb3ddaf3204b
ardin <64200985+ArdinDokht@users.noreply.github.com>
2024-11-13 18:04:02 +0330

    add Date Tag !!
    add date for tagging to the everyday backup !

--- container_backup.sh
+++ container_backup.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
-CONTAINER_ID="313490859a8d"
-NEW_IMAGE_NAME="nginx_proxy_db:21-8-1403"
-EXPORT_FILE="/tmp/nginx_db_image_backup.tar"
-
-docker commit "$CONTAINER_ID" "$NEW_IMAGE_NAME"
-echo "Container $CONTAINER_ID committed as image $NEW_IMAGE_NAME"
-
-
-docker save -o "$EXPORT_FILE" "$NEW_IMAGE_NAME"
-echo "Image $NEW_IMAGE_NAME saved as $EXPORT_FILE"
-
-
 BACKUP_SERVER_USER="ubuntu"
-BACKUP_SERVER_IP="192.168.110.54"
+BACKUP_SERVER_IP="192.168.110.70"
 BACKUP_SERVER_PATH="/tmp/"
 
-scp "$EXPORT_FILE" "$BACKUP_SERVER_USER@$BACKUP_SERVER_IP:$BACKUP_SERVER_PATH"
-echo "Image file sent to backup server at $BACKUP_SERVER_IP:$BACKUP_SERVER_PATH"
+DATE_TAG=$(date +"%Y-%m-%d")
+
+
+ACTIVE_CONTAINERS=$(docker ps -q)
+
+for CONTAINER_ID in $ACTIVE_CONTAINERS; do
+
+  NEW_IMAGE_NAME="backup_image_${CONTAINER_ID}_$DATE_TAG"
+  EXPORT_FILE="/tmp/${NEW_IMAGE_NAME}.tar"
+
+  docker commit "$CONTAINER_ID" "$NEW_IMAGE_NAME"
+  echo "Container $CONTAINER_ID committed as image $NEW_IMAGE_NAME"
+
+  docker save -o "$EXPORT_FILE" "$NEW_IMAGE_NAME"
+  echo "Image $NEW_IMAGE_NAME saved as $EXPORT_FILE"
+
+  scp "$EXPORT_FILE" "$BACKUP_SERVER_USER@$BACKUP_SERVER_IP:$BACKUP_SERVER_PATH"
+  echo "Image file $EXPORT_FILE sent to backup server at $BACKUP_SERVER_IP:$BACKUP_SERVER_PATH"
+done
